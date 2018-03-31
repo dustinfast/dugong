28bee38f2e3b2de516d06ee1e58ab059ef59b77f
Dustin Fast <dustin.fast@hotmail.com>
2018-03-31 14:30:25 -0800

    Added debug outout -DF

---
+++ uselexer.lua
@@ -0,0 +1,38 @@
+#!/usr/bin/env lua
+-- uselexer.lua
+-- Glenn G. Chappell
+-- 6 Feb 2018
+--
+-- For CS F331 / CSCE A331 Spring 2018
+-- Simple Main Program for lexer Module
+-- Requires lexit.lua
+
+lexer = require "lexit"
+
+
+-- Our "program", which is sent to the lexer
+-- Change this string and see what happens.
+program = "-1-1-1-1"
+
+-- Above, "\a" is an alarm character. It may make a beep sound when
+-- printed. It is included as an example of an illegal character.
+
+
+-- Print the input ("program")
+io.write("*** Lexer Input:\n")
+io.write(program)
+io.write("\n\n")
+
+-- Lex away and print the output as we go
+io.write("*** Lexer Output:\n")
+n = 1
+for lexstr, cat in lexer.lex(program) do
+    print('\n')
+    n = n+1
+    lexer.preferOp()
+    -- if n == 2 then lexer.preferOp() end
+    print(lexstr, lexer.catnames[cat])
+end
+
+
+
